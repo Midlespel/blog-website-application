60db89f857937671ff2d13f25d6f5ba2deaf1dbb
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-09 19:39:07 +0700

    Add carousel section in index.html

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
     $(".toggle").click(function () {
         $(".nav-menubar, .social").slideToggle("slow");
     });
+
+
+    // Owl Carousel for favorites blog post
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+    });
 });
